ed2b9dbe21e700f498366e164b47552bad6ae2b0
atikus <atilla@allita.com>
2018-05-22 10:13:22 +0300

    versiyon 1

---
+++ MyApplication2/app/src/main/java/tr/com/dna360/myapplication2/FragmentA.java
@@ -0,0 +1,65 @@
+package tr.com.dna360.myapplication2;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class FragmentA extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView tv = getActivity().findViewById(R.id.textview1);
+        try {
+            if (tv != null)
+                tv.setText("Naber");
+            else
+                Log.e("atilla", "hata 2");
+        } catch (Exception e) {
+            Log.e("atilla", e.toString());
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            InputStream fis = getContext().getAssets().open("hello.txt");
+
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        tv.setText(sb.toString());
+
+    }
+
+
+}
--- MyApplication2/app/src/main/java/tr/com/dna360/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/tr/com/dna360/myapplication2/MainActivity.java
@@ -1,7 +1,11 @@
 package tr.com.dna360.myapplication2;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager man = getFragmentManager();
+                FragmentTransaction ft = man.beginTransaction();
+
+                FragmentA a = new FragmentA();
+
+                ft.add(R.id.layout1, a, "FragA");
+
+                ft.commit();
+
+            }
+        });
     }
 }
